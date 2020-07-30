4766699bf13d26352cc6e8b9f7d812092a27fb32
Arunn Chanthirakanthan <arunnie92@Arunns-MacBook-Pro-2.local>
2020-07-29 17:15:51 -0700

    read and save json data

--- main/main.go
+++ main/main.go
@@ -1,13 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
 )
 
-type Product struct {
-	skuId   string `json:"currentProductSku"`
-	productDescription string `json:"productDescription"`
-	purchasedDate string `json:"currentProductSku"`
-	// expiredDate
+type product struct {
+	SkuID              string `json:"currentProductSku"`
+	ProductDescription string `json:"productDescription"`
+	TermBeginDate      string `json:"termBeginDate"`
+	TermEndDate        string `json:"termEndDate"`
+}
+
+type products []product
+
+func main() {
+	jsonFile, jsonFilErr := os.Open("../data_2.json")
+	if jsonFilErr != nil {
+		fmt.Println(jsonFilErr)
+		return
+	}
+
+	fmt.Println("Successfully Opened data")
+
+	defer jsonFile.Close()
+
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	var items products
+
+	err := json.Unmarshal(byteValue, &items)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println("here")
+	fmt.Println(items[0].SkuID)
 }
