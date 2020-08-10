8e52969422bdf0a496688241b8dd2a59147e89ca
Arunn Chanthirakanthan <arunnie92@Arunns-MacBook-Pro-2.local>
2020-08-09 23:20:33 -0700

    multiple things

--- main/main.go
+++ main/main.go
@@ -1,11 +1,29 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/speps/go-hashids"
 )
+
+type hashID string
+
+func newHash() *hashids.HashID {
+	hd := hashids.NewData()
+	hd.Salt = "id"
+	hd.MinLength = 30
+	h, _ := hashids.NewWithData(hd)
+	// e, _ := *h.Encode([]int{45, 434, 1313, 99})
+	// fmt.Println(e)
+	// d, _ := h.DecodeWithError(e)
+	// fmt.Println(d)
+
+	return h
+}
 
 type product struct {
 	SkuID              string `json:"currentProductSku"`
@@ -14,16 +32,30 @@ type product struct {
 	TermEndDate        string `json:"termEndDate"`
 }
 
+type userInfo struct {
+	Email string
+	// Password string // gmail password
+}
+
 type products []product
 
 func main() {
+	reader := bufio.NewReader(os.Stdin)
+	var user userInfo
+
+	fmt.Print("Please enter your emaill-> ")
+	email, _ := reader.ReadString('\n')
+	user.Email = email
+
+	// TODO: validate email
+
+	fmt.Println("Uploading Geek Squad GSP JSON file... ")
 	jsonFile, jsonFilErr := os.Open("../data_2.json")
 	if jsonFilErr != nil {
 		fmt.Println(jsonFilErr)
 		return
 	}
-
-	fmt.Println("Successfully Opened data")
+	fmt.Println("Successfully Geek Squad GSP JSON file...")
 
 	defer jsonFile.Close()
 
@@ -37,6 +69,12 @@ func main() {
 		return
 	}
 
-	fmt.Println("here")
 	fmt.Println(items[0].SkuID)
+
+	for index, item := range items {
+		fmt.Println(index)
+
+		fmt.Println(item.SkuID)
+	}
+	// fmt.Println(toHash(items[0].SkuID))
 }
